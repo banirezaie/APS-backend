4780eb23b098e90698f6b2b809c83b0a15729b16
BANI GIT <bani.rezaie@gmail.com>
2021-08-21 21:33:59 +0100

    express validator added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-//@route GET api/users
+//@route POST api/users
 //@desc Test route
 //@access Public
-router.get("/", (req, res) => {
-  res.send("Users Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Email is required").isEmail(),
+    check(
+      "password",
+      "Password is required, the length should be at least 6 characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    //see if user exists
+    try {
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+    res.send("Users Route");
+  }
+);
 
 module.exports = router;
