603ec9b196dbd0bfae903b7aabaf7281c8f69876
BANI GIT <bani.rezaie@gmail.com>
2021-09-05 22:41:59 +0100

    bug fixed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,6 @@ const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
-const { populate } = require("../../models/Profile");
 
 //@route GET api/profile/me
 //@desc  Get current user's profile
